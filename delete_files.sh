8f69fd83609de863f8dbc06947d430e3be33733e
metamoq <werewolf13@list.ru>
2023-11-30 20:13:14 +0300

    delete all files and folders

--- delete_files.sh
+++ delete_files.sh
@@ -2,11 +2,11 @@
 
 for folder in {1..50}; do
     cd "Folder_$folder" || continue  # Переход в каждую из 50 папок
-    for file_num in {31..44}; do
+    for file_num in {0..50}; do
         file_name="file_$file_num.html"
         git rm "$file_name"
     done
-    git commit -m "Deleted files with numbers 31-44 from directories $folder"
+    git commit -m "Deleted files with numbers 0-50 from directories $folder"
     git push origin main  # Отправить изменения в удаленный репозиторий
     cd ..  # Перейти обратно в основную директорию
 done
